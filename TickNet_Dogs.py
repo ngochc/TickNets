6641a6b7d61b346db0533a1bb60a32c1086921b7
Ngoc Hoang <74035683+ngochc@users.noreply.github.com>
2024-12-31 19:16:59 +0700

    Add log function and export output to csv

--- TickNet_Dogs.py
+++ TickNet_Dogs.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import argparse
+import csv
 import sys
 import os
-#import inspect
+# import inspect
 import time
 import torch
 import torch.nn
@@ -13,29 +14,42 @@ import torch.utils.data
 import torchvision.transforms
 import torchvision.datasets
 
-#from pathlib import Path
-#sys.path.append(str(Path('.').absolute().parent))
+# from pathlib import Path
+# sys.path.append(str(Path('.').absolute().parent))
 from models.datasets import *
 from models.TickNet import *
 import writeLogAcc as wA
+
 
 def get_args():
     """
     Parse the command line arguments.
     """
-    parser = argparse.ArgumentParser(description='TickNet training script for cifar and StanfordDogs datasets.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)    
-    parser.add_argument('-r', '--data-root', type=str, default='../../../datasets/StanfordDogs', help='Dataset root path.')
-    #parser.add_argument('-d', '--dataset', choices=['cifar10', 'cifar100', 'dogs'], required=True, help='Dataset name.')
-    parser.add_argument('-d', '--dataset', type=str, choices=['cifar10', 'cifar100', 'dogs'], default='dogs', help='Dataset name.')
-    parser.add_argument('--download', action='store_true', help='Download the specified dataset before running the training.')    
-    parser.add_argument('-g', '--gpu-id', default=1, type=int, help='ID of the GPU to use. Set to -1 to use CPU.')
-    parser.add_argument('-j', '--workers', default=4, type=int, help='Number of data loading workers.')
-    parser.add_argument('-b', '--batch-size', default=64, type=int, help='Batch size.')        
-    parser.add_argument('-e', '--epochs', default=200, type=int, help='Number of total epochs to run.')
-    parser.add_argument('-l', '--learning-rate', default=0.1, type=float, help='Initial learning rate.')
-    parser.add_argument('-s', '--schedule', nargs='+', default=[100, 150, 180], type=int, help='Learning rate schedule (epochs after which the learning rate should be dropped).')    
-    parser.add_argument('-m', '--momentum', default=0.9, type=float, help='SGD momentum.')
-    parser.add_argument('-w', '--weight-decay', default=1e-4, type=float, help='SGD weight decay.')    
+    parser = argparse.ArgumentParser(description='TickNet training script for cifar and StanfordDogs datasets.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-r', '--data-root', type=str,
+                        default='../../../datasets/StanfordDogs', help='Dataset root path.')
+    # parser.add_argument('-d', '--dataset', choices=['cifar10', 'cifar100', 'dogs'], required=True, help='Dataset name.')
+    parser.add_argument('-d', '--dataset', type=str, choices=[
+                        'cifar10', 'cifar100', 'dogs'], default='dogs', help='Dataset name.')
+    parser.add_argument('--download', action='store_true',
+                        help='Download the specified dataset before running the training.')
+    parser.add_argument('-g', '--gpu-id', default=1, type=int,
+                        help='ID of the GPU to use. Set to -1 to use CPU.')
+    parser.add_argument('-j', '--workers', default=4, type=int,
+                        help='Number of data loading workers.')
+    parser.add_argument('-b', '--batch-size', default=64,
+                        type=int, help='Batch size.')
+    parser.add_argument('-e', '--epochs', default=200,
+                        type=int, help='Number of total epochs to run.')
+    parser.add_argument('-l', '--learning-rate', default=0.1,
+                        type=float, help='Initial learning rate.')
+    parser.add_argument('-s', '--schedule', nargs='+', default=[
+                        100, 150, 180], type=int, help='Learning rate schedule (epochs after which the learning rate should be dropped).')
+    parser.add_argument('-m', '--momentum', default=0.9,
+                        type=float, help='SGD momentum.')
+    parser.add_argument('-w', '--weight-decay', default=1e-4,
+                        type=float, help='SGD weight decay.')
     parser.add_argument('--evaluate', dest='evaluate', action='store_true',
                         help='evaluate model on validation set')
     return parser.parse_args()
@@ -43,13 +57,15 @@ def get_args():
 
 def get_device(args):
     """
-    Determine the device to use for the given arguments.
+    Determine the device to use for the given arguments, including MPS for Mac Silicon.
     """
-    if args.gpu_id >= 0:
+    if args.gpu_id >= 0 and torch.cuda.is_available():
         return torch.device('cuda:{}'.format(args.gpu_id))
+    elif torch.backends.mps.is_available():
+        return torch.device('mps')
     else:
         return torch.device('cpu')
-    
+
 
 def get_data_loader(args, train):
     """
@@ -67,13 +83,13 @@ def get_data_loader(args, train):
             transform = torchvision.transforms.Compose([
                 torchvision.transforms.ToTensor(),
             ])
-    
+
         # cifar10 vs. cifar100
         if args.dataset == 'cifar10':
             dataset_class = torchvision.datasets.CIFAR10
         else:
             dataset_class = torchvision.datasets.CIFAR100
-            
+
     elif args.dataset in ('dogs',):
         # select transforms based on train/val
         if train:
@@ -90,20 +106,23 @@ def get_data_loader(args, train):
                 torchvision.transforms.CenterCrop(224),
                 torchvision.transforms.ToTensor()
             ])
-    
-        #dataset_class = models.datasets.StanfordDogs
+
+        # dataset_class = models.datasets.StanfordDogs
         dataset_class = StanfordDogs
-    
+
     else:
-        raise NotImplementedError('Can\'t determine data loader for dataset \'{}\''.format(args.dataset))
-    
+        raise NotImplementedError(
+            'Can\'t determine data loader for dataset \'{}\''.format(args.dataset))
+
     # trigger download only once
     if args.download:
-        dataset_class(root=args.data_root, train=train, download=True, transform=transform)
+        dataset_class(root=args.data_root, train=train,
+                      download=True, transform=transform)
 
     # instantiate dataset class and create data loader from it
-    dataset = dataset_class(root=args.data_root, train=train, download=False, transform=transform)
-    return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True if train else False, num_workers=args.workers)    
+    dataset = dataset_class(root=args.data_root, train=train,
+                            download=False, transform=transform)
+    return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True if train else False, num_workers=args.workers)
 
 
 def calculate_accuracy(output, target):
@@ -126,7 +145,7 @@ def run_epoch(train, data_loader, model, criterion, optimizer, n_epoch, args, de
     else:
         model.eval()
         torch.set_grad_enabled(False)
-    
+
     batch_count = len(data_loader)
     losses = []
     accs = []
@@ -135,7 +154,7 @@ def run_epoch(train, data_loader, model, criterion, optimizer, n_epoch, args, de
         target = target.to(device)
 
         output = model(images)
-        loss = criterion(output, target)        
+        loss = criterion(output, target)
 
         # record loss and measure accuracy
         loss_item = loss.item()
@@ -148,12 +167,60 @@ def run_epoch(train, data_loader, model, criterion, optimizer, n_epoch, args, de
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-    
+
         if (n_batch % 10) == 0:
-            print('[{}]  epoch {}/{},  batch {}/{},  loss_{}={:.5f},  acc_{}={:.2f}%'.format('train' if train else ' val ', n_epoch + 1, args.epochs, n_batch + 1, batch_count, "train" if train else "val", loss_item, "train" if train else "val", 100.0 * acc))
-    
+            print('[{}]  epoch {}/{},  batch {}/{},  loss_{}={:.5f},  acc_{}={:.2f}%'.format('train' if train else ' val ', n_epoch + 1,
+                  args.epochs, n_batch + 1, batch_count, "train" if train else "val", loss_item, "train" if train else "val", 100.0 * acc))
+
     return (sum(losses) / len(losses), sum(accs) / len(accs))
-            
+
+
+def get_unique_file_path(result_dir, base_filename, total_epochs):
+    """
+    Create a unique file path by adding an index at the end of the file name if the file already exists.
+    """
+    result_file_path = os.path.join(
+        result_dir, f'{base_filename}_epochs{total_epochs}.csv')
+    index = 1
+    while os.path.isfile(result_file_path):
+        result_file_path = os.path.join(
+            result_dir, f'{base_filename}_epochs{total_epochs}_{index}.csv')
+        index += 1
+    return result_file_path
+
+
+def initialize_csv_file(result_dir, total_epochs):
+    """
+    Create a CSV file with a unique name and write the header.
+    """
+    base_filename = 'result'
+    result_file_path = get_unique_file_path(
+        result_dir, base_filename, total_epochs)
+    header = ['Epoch', 'Train Loss', 'Train Accuracy',
+              'Validation Loss', 'Validation Accuracy']
+
+    # Create directory if it does not exist
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+
+    with open(result_file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(header)
+
+    return result_file_path
+
+
+def log_results_to_csv(result_file_path, current_epoch, train_loss, train_accuracy, val_loss, val_accuracy):
+    """
+    Log the results to a CSV file.
+    """
+    result_row = [current_epoch + 1, train_loss,
+                  train_accuracy, val_loss, val_accuracy]
+
+    with open(result_file_path, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(result_row)
+
 
 def main():
     """
@@ -164,82 +231,122 @@ def main():
     args.gpu_id = 1
     device = get_device(args)
     print('Using device {}'.format(device))
-    
-    # print model with parameter and FLOPs counts    
-    torch.autograd.set_detect_anomaly(True)     
-    
-    #arr_typesize = ['large', 'small']
-    arr_typesize = ['small']
-    for typesize in arr_typesize:    
-        strmode = 'StanfordDogs_TickNet_' + typesize + '_SE'  
+
+    # print model with parameter and FLOPs counts
+    torch.autograd.set_detect_anomaly(True)
+
+    arr_typesize = ['basic', 'small', 'large']
+    for typesize in arr_typesize:
+        strmode = 'StanfordDogs_TickNet_' + typesize + '_SE'
         pathout = './checkpoints/' + strmode
         filenameLOG = pathout + '/' + strmode + '.txt'
         if not os.path.exists(pathout):
             os.makedirs(pathout)
+
+        # Directory for logging results to CSV
+        result_dir = './report/' + 'StanfordDogs_' + typesize
+        if not os.path.exists(result_dir):
+            os.makedirs(result_dir)
+
+        # Initialize CSV file once for the entire run
+        result_file_path = initialize_csv_file(result_dir, args.epochs)
+
         # get model
-        
-        model = build_TickNet(120, typesize=typesize, cifar=False)                
+        model = build_TickNet(120, typesize=typesize, cifar=False)
         model = model.to(device)
-        
+
         print(model)
-        
         print('Number of model parameters: {}'.format(
-        sum([p.data.nelement() for p in model.parameters()])))
-    
+            sum([p.data.nelement() for p in model.parameters()])))
+
         # define loss function and optimizer
         criterion = torch.nn.CrossEntropyLoss().to(device)
-        optimizer = torch.optim.SGD(params=model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=args.schedule, gamma=0.1)
-        
+        optimizer = torch.optim.SGD(
+            params=model.parameters(),
+            lr=args.learning_rate,
+            momentum=args.momentum,
+            weight_decay=args.weight_decay
+        )
+
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer=optimizer,
+            milestones=args.schedule,
+            gamma=0.1
+        )
+
         # get train and val data loaders
         train_loader = get_data_loader(args=args, train=True)
         val_loader = get_data_loader(args=args, train=False)
-        
+
         if args.evaluate:
             pathcheckpoint = "./checkpoints/StanfordDogs/small/model_best.pth"
             if os.path.isfile(pathcheckpoint):
                 print("=> loading checkpoint '{}'".format(pathcheckpoint))
                 checkpoint = torch.load(pathcheckpoint)
-                model.load_state_dict(checkpoint['model_state_dict'])                
+                model.load_state_dict(checkpoint['model_state_dict'])
                 del checkpoint
             else:
                 print("=> no checkpoint found at '{}'".format(pathcheckpoint))
                 return
             m = time.time()
-            (loss_val, acc_val) = run_epoch(train=False, data_loader=val_loader, model=model, criterion=criterion, optimizer=None, n_epoch=0, args=args, device=device)
-            print('[{}], loss_{}={:.5f},  acc_{}={:.2f}%'.format(' validating: ', "val", loss_val, "val", 100.0 * acc_val))
+            (val_loss, val_accuracy) = run_epoch(train=False, data_loader=val_loader, model=model,
+                                                 criterion=criterion, optimizer=None, n_epoch=0, args=args, device=device)
+            print(
+                f'[ validating: ], loss_val={val_loss:.5f}, acc_val={100.0 * val_accuracy:.2f}%'
+            )
             n = time.time()
             print((n-m)/3600)
             return
+
         # for each epoch...
-        acc_val_max = None
-        acc_val_argmax = None
-        for n_epoch in range(args.epochs):
+        val_accuracy_max = None
+        val_accuracy_argmax = None
+        for current_epoch in range(args.epochs):
             current_learning_rate = optimizer.param_groups[0]['lr']
-            print('Starting epoch {}/{},  learning_rate={}'.format(n_epoch + 1, args.epochs, current_learning_rate))
-            
+            print(
+                f'Starting epoch {current_epoch + 1}/{args.epochs}, learning_rate={current_learning_rate}'
+            )
+
             # train
-            (loss_train, acc_train) = run_epoch(train=True, data_loader=train_loader, model=model, criterion=criterion, optimizer=optimizer, n_epoch=n_epoch, args=args, device=device)
-    
+            (train_loss, train_accuracy) = run_epoch(train=True, data_loader=train_loader, model=model,
+                                                     criterion=criterion, optimizer=optimizer, n_epoch=current_epoch, args=args, device=device)
+
             # validate
-            (loss_val, acc_val) = run_epoch(train=False, data_loader=val_loader, model=model, criterion=criterion, optimizer=None, n_epoch=n_epoch, args=args, device=device)
-            if (acc_val_max is None) or (acc_val > acc_val_max):
-                acc_val_max = acc_val
-                acc_val_argmax = n_epoch
-                torch.save({"model_state_dict": model.state_dict()}, pathout + '/' + 'checkpoint_epoch{:>04d}_{:.2f}.pth'.format(n_epoch + 1,100.0 * acc_val_max))
-    
+            (val_loss, val_accuracy) = run_epoch(train=False, data_loader=val_loader, model=model,
+                                                 criterion=criterion, optimizer=None, n_epoch=current_epoch, args=args, device=device)
+            if (val_accuracy_max is None) or (val_accuracy > val_accuracy_max):
+                val_accuracy_max = val_accuracy
+                val_accuracy_argmax = current_epoch
+                torch.save(
+                    {"model_state_dict": model.state_dict()},
+                    f'{pathout}/checkpoint_epoch{current_epoch + 1:>04d}_{100.0 * val_accuracy_max:.2f}.pth'
+                )
+
             # adjust learning rate
             scheduler.step()
-    
+
             # save the model weights
-            #torch.save({"model_state_dict": model.state_dict()}, 'checkpoint_epoch{:>04d}.pth'.format(n_epoch + 1))
-            
+            # torch.save({"model_state_dict": model.state_dict()}, 'checkpoint_epoch{:>04d}.pth'.format(n_epoch + 1))
+
             # print epoch summary
-            line = 'Epoch {}/{} summary:  loss_train={:.5f},  acc_train={:.2f}%,  loss_val={:.2f},  acc_val={:.2f}% (best: {:.2f}% @ epoch {})'.format(n_epoch + 1, args.epochs, loss_train, 100.0 * acc_train, loss_val, 100.0 * acc_val, 100.0 * acc_val_max, acc_val_argmax + 1)
+            line = (
+                f'Epoch {current_epoch + 1}/{args.epochs} summary: '
+                f'loss_train={train_loss:.5f}, '
+                f'acc_train={100.0 * train_accuracy:.2f}%, '
+                f'loss_val={val_loss:.2f}, '
+                f'acc_val={100.0 * val_accuracy:.2f}% '
+                f'(best: {100.0 * val_accuracy_max:.2f}% @ epoch {val_accuracy_argmax + 1})'
+            )
+
             print('=' * len(line))
             print(line)
             print('=' * len(line))
-            wA.writeLogAcc(filenameLOG,line)
+            wA.writeLogAcc(filenameLOG, line)
+
+            # Log results to CSV
+            log_results_to_csv(result_file_path, current_epoch,
+                               train_loss, train_accuracy, val_loss, val_accuracy)
+
 
 if __name__ == '__main__':
     try:
--- models/datasets.py
+++ models/datasets.py
@@ -10,14 +10,14 @@ class StanfordDogs(torchvision.datasets.VisionDataset):
     """
     Dataset class for the StanfordDogs (aka ImageNetDogs) dataset
     (http://vision.stanford.edu/aditya86/ImageNetDogs/).
-    
+
     This class is PyTorch/torchvision compatible.
-    
+
     The `root` dir must contain the raw files `images.tar` and `lists.tar`,
     which are available from the URL above. They can also be downloaded
     automatically by setting `download=True`.
     """
-    
+
     sources = (
         {
             "url": "http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar",
@@ -32,20 +32,31 @@ class StanfordDogs(torchvision.datasets.VisionDataset):
             "extracted_filenames": ("file_list.mat", "test_list.mat", "train_list.mat"),
         },
     )
-    
-    def __init__(self, root, train=True, transforms=None, transform=None, target_transform=None, download=False, loader=torchvision.datasets.folder.default_loader):
-        super().__init__(root=root, transforms=transforms, transform=transform, target_transform=target_transform)
+
+    def __init__(
+        self,
+        root,
+        train=True,
+        transforms=None,
+        transform=None,
+        target_transform=None,
+        download=False,
+        loader=torchvision.datasets.folder.default_loader
+    ):
+        super().__init__(root=root, transforms=transforms,
+                         transform=transform, target_transform=target_transform)
         self.loader = loader
-        
+
         self.train = train
-        
+
         if download:
             self.download()
-            
+
         self.unique_class_names = self.read_unique_class_names()
         self.image_filenames = self.read_image_filenames()
-        self.targets = tuple(self.get_class_index_from_image_filename(image_filename) for image_filename in self.image_filenames)
-        
+        self.targets = tuple(
+            self.get_class_index_from_image_filename(image_filename) for image_filename in self.image_filenames)
+
     @staticmethod
     def read_file_list_from_mat(filename):
         """
@@ -55,29 +66,32 @@ class StanfordDogs(torchvision.datasets.VisionDataset):
         import scipy.io
         mat = scipy.io.loadmat(filename)
         return [value[0] for value in mat["file_list"][:, 0]]
-    
+
     def read_unique_class_names(self):
         """
         Load the class names from the file list and return all unique values
         as a tuple.
         """
-        image_filenames = self.read_file_list_from_mat(filename=os.path.join(self.root, "file_list.mat"))
+        image_filenames = self.read_file_list_from_mat(
+            filename=os.path.join(self.root, "file_list.mat"))
         class_names = set()
         for image_filename in image_filenames:
-            class_name = self.get_class_name_from_image_filename(image_filename=image_filename)
+            class_name = self.get_class_name_from_image_filename(
+                image_filename=image_filename)
             class_names.add(class_name)
         return tuple(sorted(class_names))
-        
+
     def get_class_name_from_image_filename(self, image_filename):
         """
         Derive the class name from the given image filename.
         """
         return os.path.basename(image_filename).split("_")[0]
-    
+
     def get_class_index_from_image_filename(self, image_filename):
-        class_name = self.get_class_name_from_image_filename(image_filename=image_filename)
+        class_name = self.get_class_name_from_image_filename(
+            image_filename=image_filename)
         return self.unique_class_names.index(class_name)
-    
+
     def read_image_filenames(self):
         """
         Read all image filenames from the dataset's list files. Varies whether
@@ -89,13 +103,15 @@ class StanfordDogs(torchvision.datasets.VisionDataset):
         else:
             list_filename = "test_list.mat"
             image_count = 8580
-        image_filenames = self.read_file_list_from_mat(filename=os.path.join(self.root, list_filename))
-        image_filenames = tuple(os.path.join(self.root, "Images", image_filename) for image_filename in image_filenames)
+        image_filenames = self.read_file_list_from_mat(
+            filename=os.path.join(self.root, list_filename))
+        image_filenames = tuple(os.path.join(
+            self.root, "Images", image_filename) for image_filename in image_filenames)
 
         assert len(image_filenames) == image_count
 
         return image_filenames
-    
+
     def __getitem__(self, index):
         """
         Return the `index`-th sample of the dataset, which is a tuple
@@ -108,16 +124,16 @@ class StanfordDogs(torchvision.datasets.VisionDataset):
         target = self.targets[index]
         if self.target_transform is not None:
             target = self.target_transform(target)
-        
+
         return (image, target)
-    
+
     def __len__(self):
         """
         Returns the number of images in this dataset. Varies whether
         `self.train` is `True` or `False`.
         """
         return len(self.image_filenames)
-    
+
     def download(self):
         """
         Download and extract the neccessary source files into the root
@@ -125,14 +141,17 @@ class StanfordDogs(torchvision.datasets.VisionDataset):
         """
         for source in self.sources:
             full_filename = os.path.join(self.root, source["filename"])
-            
+
             # download
-            torchvision.datasets.utils.download_url(url=source["url"], root=self.root, filename=source["filename"], md5=source["md5"])
-                
+            torchvision.datasets.utils.download_url(
+                url=source["url"], root=self.root, filename=source["filename"], md5=source["md5"])
+
             # extract
             if not all(os.path.exists(os.path.join(self.root, extracted_filename)) for extracted_filename in source["extracted_filenames"]):
-                print("Extracting '{}' to '{}'".format(source["filename"], self.root))
+                print("Extracting '{}' to '{}'".format(
+                    source["filename"], self.root))
                 with tarfile.open(full_filename, "r") as tar:
                     tar.extractall(path=self.root)
             else:
-                print("File '{}' was already extracted, skipped extraction".format(source["filename"]))
+                print("File '{}' was already extracted, skipped extraction".format(
+                    source["filename"]))
