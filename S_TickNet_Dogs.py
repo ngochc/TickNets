9ce5c083d215e7ce95eb236b23eff8407d8e1f13
Ngoc Hoang <ngocit09@gmail.com>
2025-01-22 10:31:19 +0700

    Update config

--- S_TickNet_Dogs.py
+++ S_TickNet_Dogs.py
@@ -58,6 +58,8 @@ def get_args():
                         help='Base directory for saving checkpoints')
     parser.add_argument('--evaluate', dest='evaluate', action='store_true',
                         help='evaluate model on validation set')
+    parser.add_argument('--config', type=int, default=0,
+                        help='Config index for experiment')
     return parser.parse_args()
 
 
@@ -196,10 +198,9 @@ def main():
 
     # Set the base directory
     arr_architecture_types = args.architecture_types
-    cf_index = 3
+    cf_index = args.config
     for typesize in arr_architecture_types:
-        typesize = 'small'
-        strmode = f'StanfordDogs_S_TickNet_{typesize}_SE_smallconfig_{cf_index}'
+        strmode = f'StanfordDogs_S_TickNet_{typesize}_SE_config_{cf_index}'
         pathout = f'{args.base_dir}/checkpoints/{strmode}'
 
         filenameLOG = pathout + '/' + strmode + '.txt'
